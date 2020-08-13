4f1c6b78be8dff8fd3e6a37e6977155a5de7c7c7
yasminekhalifa <yasminaekhalifa@gmail.com>
2020-08-13 13:40:26 +1000

    goodguys

---
+++ ETL Project/Schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE goodguys (
+  model varchar(30) PRIMARY KEY,
+  retailer varchar(30),
+  category varchar(30),
+	currency varchar(30),
+	brand varchar(30),
+	name varchar(100),
+	size varchar(30),
+	price varchar(30),
+	image varchar(200)
+);
